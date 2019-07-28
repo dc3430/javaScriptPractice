2e832497fb37c56a5abb605cfff4d6f9d4f5d0ac
Daniel <cermak222@gmail.com>
2019-07-28 08:30:21 -0500

    added first commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+
+// let cat = { // creating an object of cat
+//     name: "Fluffy", //property
+//     age: 4, //property
+//     eyeColor: "blue", //property
+//     meow() { // this is a method, 'is a function', that live in side a object. we don't need to call the function
+//         alert("meowwww")
+//     }
+// };
+
+// cat.meow(); //calling the cat function
+
+
+
+
+// let myWords = ["red", "orange", "blue"] //creating arrays
+// let myFavoriteNumber = [6, 3, 5, 7, 8, 0]
+// let myPets = [{name: "Meowsalo", species: "cat"}, {name: "Barksalot", species: "dog"}] // collection of values
+
+// myWords.push("green"); //push add to the array
+// console.log(myWords);
+
+// myWords.splice(2, 1);  //you will be removing with splice (idx , howManyItems)
+// console.log(myWords);
+
+// console.log(myFavoriteNumber[2]) //square bracket to look up by idx
+
+// console.log(myPets[1].species)
+
+
+
+
+
+// let myName = "Daniel"
+// console.log(myName.toUpperCase); //setting all text to upper cse
+
+// let myNumber = 7.89
+// console.log(myNumber.toFixed()); //making a hole number
+
